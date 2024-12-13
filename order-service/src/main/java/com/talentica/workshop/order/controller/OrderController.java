8295401179b6c094f5a823b1a0fd8bd73440846a
vipul-tal <vipul.sharma@talentica.com>
2024-12-13 12:17:21 +0000

    soln-session1

--- order-service/src/main/java/com/talentica/workshop/order/controller/OrderController.java
+++ order-service/src/main/java/com/talentica/workshop/order/controller/OrderController.java
@@ -6,7 +6,7 @@ import com.talentica.workshop.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestControllerdock
 @RequestMapping("/order")
 public class OrderController {
 
